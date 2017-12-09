56d4dc9fa7ebdad6345a984625b122b2d4651fff
Belene <be23mg@gmail.com>
2017-12-09 13:55:55 +0100

    [Add]remove class hidden when click button and add hidden click save name

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
 'use strict';
 
+var number = 3;
+
+var saveNumberButton = document.querySelector('#save-number');
+saveNumberButton.addEventListener('click', function() {
+  var inputNumber = document.querySelector('.user-number');
+  var track = document.querySelector('.track');
+  if (number === parseInt(inputNumber.value)) {
+    track.textContent = 'ACERTADO';
+    document.querySelector('.insert-name').classList.remove('hidden');
+  } else if (number < parseInt(inputNumber.value)) {
+    track.textContent = ('DEMASIADO ALTO');
+  } else {
+    track.textContent = ('DEMASIADO BAJO');
+  }
+});
+
+
+
+
 var saveNameButton = document.querySelector('#save-name');
 saveNameButton.addEventListener('click', function() {
+    document.querySelector('.insert-name').classList.add('hidden');
   var inputName = document.querySelector('.user-name');
   registerPlayer(inputName.value, 2);
 });
@@ -14,5 +34,6 @@ function registerPlayer(name, attempts) {
   } else {
     item.textContent = name + ' - ' + attempts + ' intentos';
   }
+
   history.appendChild(item);
 }
