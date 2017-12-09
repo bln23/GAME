701d1a2ed7de06289023aaae00e9c6b7cf06ad17
Belene <be23mg@gmail.com>
2017-12-09 12:42:59 +0100

    [Changes]Add user name in history + attempts

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 'use strict';
 
+var saveNameButton = document.querySelector('#save-name');
+saveNameButton.addEventListener('click', function() {
+  var inputName = document.querySelector('.user-name');
+  registerPlayer(inputName.value, 2);
+});
+
 function registerPlayer(name, attempts) {
   var history = document.querySelector('.history ul');
   var item = document.createElement('li');
-  item.textContent = name + ' - ' + attempts + ' intentos';
+  if (attempts === 1) {
+    item.textContent = name + ' - ' + attempts + ' intento';
+  } else {
+    item.textContent = name + ' - ' + attempts + ' intentos';
+  }
   history.appendChild(item);
 }
