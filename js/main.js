d07b8732ed985d24845f2682db98f590f985255a
Belene <be23mg@gmail.com>
2017-12-09 18:22:37 +0100

    [Change]Verion finale game

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 'use strict';
 
-var number = 3;
+function generateRandomNumber() {
+  return Math.ceil(Math.random() * 100);
+}
+
+var number = generateRandomNumber();
 
 var attempts = 0;
 var inputNumber = document.querySelector('.user-number');
@@ -23,9 +27,15 @@ saveNumberButton.addEventListener('click', function() {
 
 var saveNameButton = document.querySelector('#save-name');
 saveNameButton.addEventListener('click', function() {
-    document.querySelector('.insert-name').classList.add('hidden');
+  document.querySelector('.insert-name').classList.add('hidden');
+  track.textContent = 'PRUEBA DE NUEVO';
   var inputName = document.querySelector('.user-name');
   registerPlayer(inputName.value, attempts);
+  attempts = 0;
+  attemptsView.textContent = 0;
+  inputNumber.value = '';
+  inputName.value = '';
+  number = generateRandomNumber();
 });
 
 function registerPlayer(name, attempts) {
@@ -38,3 +48,21 @@ function registerPlayer(name, attempts) {
   }
   history.appendChild(item);
 }
+
+// var saveNameButton = document.querySelector('#save-name');
+// saveNameButton.addEventListener('click', function() {
+//     document.querySelector('.insert-name').classList.add('hidden');
+//   var inputName = document.querySelector('.user-name');
+//   registerPlayer({name: inputName.value, attempts: attempts});
+// });
+//
+// function registerPlayer(player) {
+//   var history = document.querySelector('.history ul');
+//   var item = document.createElement('li');
+//   if (player.attempts === 1) {
+//     item.textContent = player.name + ' - ' + player.attempts + ' intento';
+//   } else {
+//     item.textContent = player.name + ' - ' + player.attempts + ' intentos';
+//   }
+//   history.appendChild(item);
+// }
