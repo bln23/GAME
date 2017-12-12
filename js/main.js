2bc0806acf2e7b2bb5a76ab2792c5669cd3778e7
Belene <be23mg@gmail.com>
2017-12-12 09:34:01 +0100

    [Add]add first version game

--- js/main.js
+++ js/main.js
@@ -24,45 +24,44 @@ saveNumberButton.addEventListener('click', function() {
   }
 });
 
-
-var saveNameButton = document.querySelector('#save-name');
-saveNameButton.addEventListener('click', function() {
-  document.querySelector('.insert-name').classList.add('hidden');
-  track.textContent = 'PRUEBA DE NUEVO';
-  var inputName = document.querySelector('.user-name');
-  registerPlayer(inputName.value, attempts);
-  attempts = 0;
-  attemptsView.textContent = 0;
-  inputNumber.value = '';
-  inputName.value = '';
-  number = generateRandomNumber();
-});
-
-function registerPlayer(name, attempts) {
-  var history = document.querySelector('.history ul');
-  var item = document.createElement('li');
-  if (attempts === 1) {
-    item.textContent = name + ' - ' + attempts + ' intento';
-  } else {
-    item.textContent = name + ' - ' + attempts + ' intentos';
-  }
-  history.appendChild(item);
-}
-
 // var saveNameButton = document.querySelector('#save-name');
 // saveNameButton.addEventListener('click', function() {
-//     document.querySelector('.insert-name').classList.add('hidden');
+//   document.querySelector('.insert-name').classList.add('hidden');
+//   track.textContent = 'PRUEBA DE NUEVO';
 //   var inputName = document.querySelector('.user-name');
-//   registerPlayer({name: inputName.value, attempts: attempts});
+//   registerPlayer(inputName.value, attempts);
+//   attempts = 0;
+//   attemptsView.textContent = 0;
+//   inputNumber.value = '';
+//   inputName.value = '';
+//   number = generateRandomNumber();
 // });
 //
-// function registerPlayer(player) {
+// function registerPlayer(name, attempts) {
 //   var history = document.querySelector('.history ul');
 //   var item = document.createElement('li');
-//   if (player.attempts === 1) {
-//     item.textContent = player.name + ' - ' + player.attempts + ' intento';
+//   if (attempts === 1) {
+//     item.textContent = name + ' - ' + attempts + ' intento';
 //   } else {
-//     item.textContent = player.name + ' - ' + player.attempts + ' intentos';
+//     item.textContent = name + ' - ' + attempts + ' intentos';
 //   }
 //   history.appendChild(item);
 // }
+
+var saveNameButton = document.querySelector('#save-name');
+saveNameButton.addEventListener('click', function() {
+    document.querySelector('.insert-name').classList.add('hidden');
+  var inputName = document.querySelector('.user-name');
+  registerPlayer({name: inputName.value, attempts: attempts});
+});
+
+function registerPlayer(player) {
+  var history = document.querySelector('.history ul');
+  var item = document.createElement('li');
+  if (player.attempts === 1) {
+    item.textContent = player.name + ' - ' + player.attempts + ' intento';
+  } else {
+    item.textContent = player.name + ' - ' + player.attempts + ' intentos';
+  }
+  history.appendChild(item);
+}
